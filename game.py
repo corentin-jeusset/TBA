da68688c8147db93cab372a2beb910f7d7b54108
theo-larrat <theo.larrat@edu.esiee.fr>
2025-12-08 16:02:15 +0000

    Add custom map and improved navigation

--- game.py
+++ game.py
@@ -30,35 +30,103 @@ class Game:
         
         # Setup rooms
 
-        forest = Room("Forest", "une forêt enchantée. Vous entendez une brise légère à travers la cime des arbres.")
+        crossing = Room("Crossing", "à un croisement de plusieurs chemin, dans une étendue verte.")
+        self.rooms.append(crossing)
+        meadow = Room("Meadow", "dans une prairie, un vent frais vous caresse.")
+        self.rooms.append(meadow)
+        forest = Room("Forest", "dans une forêt, un brouillard gêne votre visibilité.")
         self.rooms.append(forest)
-        tower = Room("Tower", "une immense tour en pierre qui s'élève au dessus des nuages.")
-        self.rooms.append(tower)
-        cave = Room("Cave", "une grotte profonde et sombre. Des voix semblent provenir des profondeurs.")
+        marshes = Room("Marshes", "dans des marécages, une odeur nauséabonde en émane.")
+        self.rooms.append(marshes)
+        lake = Room("Lake", "au bord d'un lac, le brouillard s'est intensifié.")
+        self.rooms.append(lake)
+        fishing_boat = Room("Fishing boat", "à coté d'un bateau de pêcheur, ce dernier est délabré.")
+        self.rooms.append(fishing_boat)
+        fishermans_hut= Room("Fishermans_hut", "dans une cabane de pêcheur, elle semble abandonée.")
+        self.rooms.append(fishermans_hut)
+        coast= Room("coast", "arrivé sur une côte, une terrez s'étend a perte de vu.")
+        self.rooms.append(coast)
+        waterfall = Room("Waterfall", "au pied d'une cascade, l'eau s'écrase bruyamment sur les rochers.")
+        self.rooms.append(waterfall)
+        ascent = Room("Ascent", "sur un chemin qui monte abruptement, le souffle commence à manquer.")
+        self.rooms.append(ascent)
+        footpath = Room("Footpath", "sur un petit sentier bien tracé, la nature est calme et apaisante.")
+        self.rooms.append(footpath)
+        crossroads = Room("Crossroads", "à un carrefour de sentiers, plusieurs directions s'offrent à vous.")
+        self.rooms.append(crossroads)
+        viewpoint = Room("Viewpoint", "à un point de vue élevé, le paysage s'étend majestueusement à l'horizon.")
+        self.rooms.append(viewpoint)
+        cave_entrance = Room("Cave_entrance", "devant l'entrée d'une sombre caverne, l'air y est frais et humide.")
+        self.rooms.append(cave_entrance)
+        waterfall_cave = Room("Waterfall_cave", "dans une caverne derrière la cascade, le bruit est assourdissant.")
+        self.rooms.append(waterfall_cave)
+        path = Room("Sentier", "sur un sentier étroit, la végétation est dense et luxuriante.")
+        self.rooms.append(path)
+        cliff_top = Room("Cliff_top", "au sommet d'une falaise, le vent est puissant et le vide impressionnant.")
+        self.rooms.append(cliff_top)
+        little_forest = Room("Little_forest", "dans un petit bois, les arbres sont jeunes et rapprochés.")
+        self.rooms.append(little_forest)
+        road = Room("road", "sur un sentier étroit et caillouteux, il monte vers une structure en hauteur.")
+        self.rooms.append(road)
+        castle_entrance = Room("Castle_entrance", "devant la grande porte d'un château en ruines, des gargouilles vous observent.")
+        self.rooms.append(castle_entrance)
+        dungeon = Room("Dungeon", "dans un cachot sombre et froid, l'air sent la moisissure et le désespoir.")
+        self.rooms.append(dungeon)
+        bedroom = Room("Bedroom", "dans une chambre poussiéreuse et luxueuse, le temps semble s'être arrêté.")
+        self.rooms.append(bedroom)
+        boss_s_office = Room("Boss_s_office", "dans un grand bureau, un fauteuil en cuir massif fait face à une cheminée éteinte.")
+        self.rooms.append(boss_s_office)
+        bridge = Room("Bridge", "sur un pont de pierre fragile, il enjambe un profond ravin.")
+        self.rooms.append(bridge)
+        balcony = Room("Balcony", "sur un balcon en ruine, vous avez une vue plongeante sur la cour intérieure du château.")
+        self.rooms.append(balcony)
+        plain = Room("Plain", "dans une vaste plaine herbeuse, le ciel est immense et dégagé.")
+        self.rooms.append(plain)
+        plateau = Room("Plateau", "sur un plateau rocheux, la végétation est rase et le vent glacial.")
+        self.rooms.append(plateau)
+        cave = Room("Cave", "dans une cave, une énorme pierre vous tombe dessus")
         self.rooms.append(cave)
-        cottage = Room("Cottage", "un petit chalet pittoresque avec un toit de chaume. Une épaisse fumée verte sort de la cheminée.")
-        self.rooms.append(cottage)
-        swamp = Room("Swamp", "un marécage sombre et ténébreux. L'eau bouillonne, les abords sont vaseux.")
-        self.rooms.append(swamp)
-        castle = Room("Castle", "un énorme château fort avec des douves et un pont levis. Sur les tours, des flèches en or massif.")
-        self.rooms.append(castle)
 
         # Create exits for rooms
 
-        forest.exits = {"N" : cave, "E" : None, "S" : castle, "O" : None}
-        tower.exits = {"N" : cottage, "E" : None, "S" : None, "O" : None}
-        cave.exits = {"N" : None, "E" : cottage, "S" : forest, "O" : None}
-        cottage.exits = {"N" : None, "E" : None, "S" : tower, "O" : cave}
-        swamp.exits = {"N" : tower, "E" : None, "S" : None, "O" : castle}
-        castle.exits = {"N" : forest, "E" : swamp, "S" : None, "O" : None}
+        crossing.exits = {"N" : little_forest, "E" : meadow, "S" : cave, "O" : waterfall}
+        meadow.exits = {"N" : None, "E" : lake, "S" : forest, "O" : crossing}
+        forest.exits = {"N" : meadow, "E" : marshes, "S" : None, "O" : None}
+        marshes.exits = {"N" : lake, "E" : fishermans_hut, "S" : None, "O" : forest}
+        lake.exits = {"N" : None, "E" : fishing_boat, "S" : marshes, "O" : meadow}
+        fishing_boat.exits = {"N" : coast, "E" : None, "S" : fishermans_hut, "O" : None}
+        fishermans_hut.exits = {"N" : fishing_boat, "E" : None, "S" : None, "O" : marshes}
+        coast.exits = {"N" : None, "E" : None, "S" : fishing_boat, "O" : road}
+        waterfall.exits = {"N" : ascent, "E" : crossing, "S" : footpath, "O" : waterfall_cave}
+        ascent.exits = {"N" : None, "E" : None, "S" : waterfall, "O" : viewpoint}
+        footpath.exits = {"N" : waterfall, "E" : None, "S" : None, "O" : crossroads}
+        viewpoint.exits = {"N" : None, "E" : ascent, "S" : crossroads, "O" : path}
+        path.exits = {"N" : cliff_top, "E" : viewpoint, "S" : waterfall_cave, "O" : None}
+        waterfall_cave.exits = {"N" : path, "E" : waterfall, "S" : cave_entrance, "O" : None}
+        cave_entrance.exits = {"N" : waterfall_cave, "E" : crossroads, "S" : None, "O" : None}
+        cliff_top.exits = {"N" : None, "E" : road, "S" : path, "O" : None}
+        road.exits = {"N" : castle_entrance, "E" : coast, "S" : little_forest, "O" : cliff_top}
+        little_forest.exits = {"N" : road, "E" : None, "S" : crossing, "O" : None}
+        castle_entrance.exits = {"N" : boss_s_office, "E" : dungeon, "S" : road, "O" : bedroom}
+        boss_s_office.exits = {"N" : None, "E" : bridge, "S" : road, "O" : balcony}
+        dungeon.exits = {"N" : bridge, "E" : None, "S" : None, "O" : castle_entrance}
+        bedroom.exits = {"N" : balcony, "E" : castle_entrance, "S" : None, "O" : None}
+        balcony.exits = {"N" : None, "E" : boss_s_office, "S" : bedroom, "O" : plateau}
+        plateau.exits = {"N" : None, "E" : balcony, "S" : None, "O" : None}
+        plain.exits = {"N" : None, "E" : None, "S" : None, "O" : bridge}
+        cave.exits = {"N" : crossing, "E" : None, "S" : None, "O" : None}
+
 
         syn = {'N':('n','nord','NORD','Nord'),'E':('e','est','EST','Est'),'S':('s','sud','SUD','Sud'),'O':('o','ouest','OUEST','Ouest')}
-        for r in (forest,tower,cave,cottage,swamp,castle): [r.exits.update({s:v for s in (syn.get(k,())+(k.lower(),k.capitalize(),k.upper()))}) for k,v in list(r.exits.items())]
+        for r in (crossing, meadow, forest, marshes, lake, fishing_boat, fishermans_hut, coast, 
+                  waterfall, ascent, footpath, crossroads, viewpoint, cave_entrance, 
+                  waterfall_cave, path, cliff_top, little_forest, road, castle_entrance, dungeon, 
+                  bedroom, boss_s_office, bridge, balcony, plain, plateau, cave): [r.exits.update({s:v for s in (syn.get(k,())+(k.lower(),k.capitalize(),k.upper()))}) for k,v in list(r.exits.items())]
 
         # Setup player and starting room
 
         self.player = Player(input("\nEntrez votre nom: "))
-        self.player.current_room = swamp
+        self.player.current_room = crossing
 
     # Play the game
     def play(self):
